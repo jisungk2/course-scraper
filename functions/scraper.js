bda082eee7927b85dd1b817cccf5feb5d26c0de2
jisungk2 <jisungk2@berkeley.edu>
2023-10-29 23:26:21 -0700

    Changing format  - Changing the format of the courseListData  - Bug with string comparison when comparing days of lecture

--- functions/scraper.js
+++ functions/scraper.js
@@ -18,52 +18,113 @@ const scrapeData = async () => {
     });
 
 
-    let courses = {courseCodeList: [], courseNameList: [], courseProfList: [], courseDepartmentList: []};
-    while (true) {
-        let courseList; 
+    let courses = {courseListData: []};
+    let i = 9
+    while (i < 10) {
 
-        const codes = await page.evaluate(() => {
+        const pageCourseInfo = await page.evaluate(() => {
+            const courseList = document.querySelectorAll('.layout-center__content .search-results .search-result');
 
-            courseList = document.querySelectorAll('.layout-center__content .search-results .search-result');
+            const pageCourses = Array.from(courseList).map((course) => {
+                const classNumber = course.querySelector('.handlebarData .result-wrapper .hbr .col-wrapper .left-col .ls-details-flex .ls-section-number'); //need to convert innertext to number
+                let colonIndex = classNumber.innerText.indexOf(':');
+                const classId = classNumber.innerText.substring(colonIndex + 1);
 
-            const courseCodes = Array.from(courseList).map((course) => {
                 const courseCode = course.querySelector('.handlebarData .result-wrapper .hbr .col-wrapper .left-col .ls-term-year-section-wrapper .ls-section-name-number-code .ls-section-wrapper .ls-section-name');
-                return courseCode.innerText;
-            })
-            return courseCodes;
-        });
-
-        const names = await page.evaluate(() => {
-
-            const courseNames = Array.from(courseList).map((course) => {
                 const courseName = course.querySelector('.handlebarData .result-wrapper .hbr .col-wrapper .left-col .ls-course-title');
-                return courseName.innerText;
-            })
-            return courseNames;
-        });
-
-        const profs = await page.evaluate(() => {
-
-            const courseProfs = Array.from(courseList).map((course) => {
                 const courseProf = course.querySelector('.handlebarData .result-wrapper .hbr .col-wrapper .left-col .ls-instructors');
-                return (courseProf) ? courseProf.innerText : 'To Be Determined';
-            })
-            return courseProfs;
-        });
-
-        const departments = await page.evaluate(() => {
-
-            const courseDepartments = Array.from(courseList).map((course) => {
+                const courseRoom = course.querySelector('.handlebarData .result-wrapper .hbr .col-wrapper .left-col .ls-details-flex .ls-meeting-details-flex .ls-meeting-details .ls-location');
                 const courseDepartment = course.querySelector('.handlebarData .result-wrapper .hbr .col-wrapper .left-col .ls-term-year-section-wrapper .ls-section-name-number-code .ls-section-dept a');
-                return courseDepartment.innerText;
+
+                const credits = course.querySelector('.handlebarData .result-wrapper .hbr .col-wrapper .left-col .ls-details-flex .ls-details-units');
+
+                let creditColon = credits.innerText.indexOf(':');
+                const units = credits.innerText.substring(creditColon + 1);
+                
+                const courseDay = course.querySelector('.handlebarData .result-wrapper .hbr .col-wrapper .left-col .ls-details-flex .ls-meeting-details-flex .ls-meeting-details .ls-days');
+                const courseTime = course.querySelector('.handlebarData .result-wrapper .hbr .col-wrapper .left-col .ls-details-flex .ls-meeting-details-flex .ls-meeting-details .ls-time');
+
+                startTimes = [];
+                endTimes = [];
+                const cleanCourses = courseDay.innerText.trim();
+                const dayList = cleanCourses.split(", ")
+
+                if (courseDay && courseTime) {
+
+                    const courseTimeClean = courseTime.innerText.trim();
+
+                    let splitIndex = courseTimeClean.indexOf('-');
+                    const startTime = courseTimeClean.substring(0, splitIndex - 4);
+                    const startTimeColon = startTime.indexOf(":");
+                    let startHour = startTime.substring(0, startTimeColon);
+                    let startMinute = startTime.substring(startTimeColon + 1);
+                    const startTimeOfDay = courseTimeClean.substring(splitIndex - 3, splitIndex - 1);
+
+
+                    const endTime = courseTimeClean.substring(splitIndex + 1, courseTimeClean.length - 3).trim();
+                    const endTimeColon = endTime.indexOf(":");
+                    let endHour = endTime.substring(0, endTimeColon);
+                    let endMinute = endTime.substring(endTimeColon + 1);
+                    const endTimeOfDay = courseTimeClean.substring(courseTimeClean.length - 2);
+
+                    for (day in dayList) {
+                        if (day.trim().localeCompare("M") == 0) {
+                            day = "MON";
+                        } else if (day.trim().localeCompare("TU") == 0) {
+                            day = "TUE";
+                        } else if (day.trim().localeCompare("W") == 0) {
+                            day = "WED";
+                        } else if (day.trim().localeCompare("TH") == 0) {
+                            day = "THU";
+                        } else if (day.trim().localeCompare("F") == 0){
+                            day = "FRI";
+                        }
+                        
+                        if (startTimeOfDay === "pm" && parseInt(startHour) < 12) {
+                            let temp = (parseInt(startHour) + 12)
+                            startHour = temp.toString();
+                        } else if (startTimeOfDay === "am" && parseInt(startHour) < 10) {
+                            startHour = "0".concat(startHour);
+                        }
+                        if (parseInt(startMinute) == 0) {
+                            startMinute = "00";
+                        }
+
+
+                        if (endTimeOfDay === "pm" && parseInt(endHour) < 12) {
+                            let temp = (parseInt(endHour) + 12)
+                            endHour = temp.toString();
+                        } else if (endTimeOfDay === "am" && parseInt(endHour) < 10) {
+                            endHour = "0".concat(endHour);
+                        }
+                        if (parseInt(endMinute) == 0) {
+                            endMinute = "00";
+                        }
+
+                        startTimes.push(day.concat(startHour).concat(startMinute));
+                        endTimes.push(day.concat(endHour).concat(endMinute));
+                    }
+                }
+
+
+                return {
+                    id: parseInt(classId),
+                    course: courseName.innerText,
+                    courseCode: courseCode.innerText,
+                    professorName: (courseProf) ? courseProf.innerText : null,
+                    courseRoom: (courseRoom) ? courseRoom.innerText : null,
+                    startTime: (courseDay && courseTime) ? startTimes[0] : null,
+                    endTime: (courseDay && courseTime) ? endTimes : null,
+                    credits: parseInt(units),
+                    type: courseDepartment.innerText,
+                    grade: null,
+                    days: dayList
+                }
             })
-            return courseDepartments;
+            return pageCourses;
         });
 
-        courses.courseDepartmentList = courses.courseDepartmentList.concat(departments);
-        courses.courseCodeList = courses.courseCodeList.concat(codes);
-        courses.courseNameList = courses.courseNameList.concat(names);
-        courses.courseProfList = courses.courseProfList.concat(profs);
+        courses.courseListData = courses.courseListData.concat(pageCourseInfo);
 
         const nextPageUrl = await page.evaluate(() => {
             const pageReference = document.querySelector('.item-list .pager .pager-next a');
@@ -83,6 +144,7 @@ const scrapeData = async () => {
             waitUntil: "domcontentloaded"
         });
 
+        i += 1
     }
 
     await browser.close();
